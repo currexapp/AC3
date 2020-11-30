5d7ef57a51e927672f2172857de0a6da84ef3591
Nirmal Badal <nirmal.badal@mavs.uta.edu>
2020-11-30 15:49:04 -0600

    Added AC4 and AC5 xml designs

---
+++ app/src/main/java/edu/example/ac3/AC5.java
@@ -0,0 +1,14 @@
+package edu.example.ac3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AC5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_c5);
+    }
+}
